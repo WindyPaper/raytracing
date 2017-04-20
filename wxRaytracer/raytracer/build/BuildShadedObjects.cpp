5fd9a5046f596e4a6945d9730346092456e2d46d
WindyPaper <windy_max@163.com>
2017-04-20 08:31:06 +0800

    Add texture.

--- wxRaytracer/raytracer/build/BuildShadedObjects.cpp
+++ wxRaytracer/raytracer/build/BuildShadedObjects.cpp
@@ -47,13 +47,14 @@
 #include "Instance.h"
 #include "SolidCylinder.h"
 #include "Checker3D.h"
+#include "TInstance.h"
 
 void 												
 World::build(void) {
 
 	int num_samples = 16;
 
-	int sample_type = 1;
+	int sample_type = 2;
 
 	switch (sample_type)
 	{
@@ -291,64 +292,49 @@ World::build(void) {
 		add_object(plane_ptr);
 	}
 		break;
-	case 3:
+	case 2:
 	{
 		int num_samples = 16;
 
-		vp.set_hres(904);
-		vp.set_vres(300);
+		vp.set_hres(600);
+		vp.set_vres(400);
 		vp.set_samples(num_samples);
 
 		tracer_ptr = new RayCast(this);
 		background_color = RGBColor(0.5);
 
 		Pinhole* pinhole_ptr = new Pinhole;
-		pinhole_ptr->set_eye(0, 125, 200);
+		pinhole_ptr->set_eye(0, 0, 100);
 		pinhole_ptr->set_lookat(0.0);
-		pinhole_ptr->set_view_distance(4400.0);
+		pinhole_ptr->set_view_distance(7500.0);
 		pinhole_ptr->compute_uvw();
 		set_camera(pinhole_ptr);
 
 		Directional* light_ptr = new Directional;
-		light_ptr->set_direction(1, 1, 0.5);
-		light_ptr->scale_radiance(4.5);
+		light_ptr->set_direction(0, 0, 1);
+		light_ptr->scale_radiance(3.0);
 		add_light(light_ptr);
 
-
-		// middle cylinder (generic)
-
 		Checker3D* checker_ptr = new Checker3D;
-		checker_ptr->set_size(1.02);
-		checker_ptr->set_color1(black);
-		checker_ptr->set_color2(white);
+		checker_ptr->set_size(0.3);
+		checker_ptr->set_color1(white);
+		checker_ptr->set_color2(black);
+
+		TInstance* scaled_checker_ptr = new TInstance(checker_ptr);
+		scaled_checker_ptr->scale(2, 1, 1);
 
 		SV_Matte* sv_matte_ptr = new SV_Matte;
-		sv_matte_ptr->set_ka(0.4);
-		sv_matte_ptr->set_kd(0.6);
-		sv_matte_ptr->set_cd(checker_ptr);
+		sv_matte_ptr->set_ka(0.8);
+		sv_matte_ptr->set_kd(0.4);
+		sv_matte_ptr->set_cd(scaled_checker_ptr);
 
-		SolidCylinder* cylinder_ptr1 = new SolidCylinder(0, 100, 100);
-		cylinder_ptr1->set_material(sv_matte_ptr);
-		add_object(cylinder_ptr1);
+		Box* box_ptr = new Box(Point3D(-1.0), Point3D(1.0));
+		box_ptr->set_material(sv_matte_ptr);
 
-
-		// right cylinder (cylinder and checkers are transformed)
-
-		Instance* transformed_cylinder_ptr1 = new Instance(new SolidCylinder(0, 100, 100));
-		transformed_cylinder_ptr1->set_material(sv_matte_ptr);
-		transformed_cylinder_ptr1->scale(10.0, 0.5, 10.0);
-		transformed_cylinder_ptr1->translate(13, 0, 0);
-		add_object(transformed_cylinder_ptr1);
-
-
-		// left cylinder (only the cylinder is transformed)
-
-		Instance* transformed_cylinder_ptr2 = new Instance(new SolidCylinder(0, 100, 100));
-		transformed_cylinder_ptr2->set_material(sv_matte_ptr);
-		transformed_cylinder_ptr2->scale(10.0, 0.5, 10.0);
-		transformed_cylinder_ptr2->translate(-13, 0, 0);
-		transformed_cylinder_ptr2->transform_texture(false);
-		add_object(transformed_cylinder_ptr2);
+		Instance* transformed_box_ptr = new Instance(box_ptr);
+		transformed_box_ptr->scale(2, 1, 1);
+		transformed_box_ptr->rotate_z(45);
+		add_object(transformed_box_ptr);
 	}
 	break;
 	default:
